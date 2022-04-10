5335a2f529c5a398b08c4fc7bbd1f5c55ac9f1b4
omkarkarale <82709945+omkarkarale@users.noreply.github.com>
2022-04-10 22:22:20 +0530

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class CreatePost extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Create Post Screen</Text>
+      </View>
+    );
+  }
+}
